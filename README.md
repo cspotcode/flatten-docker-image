2cedf626cdfc69dedbe751721417d50863875019
Andrew Bradley <cspotcode@gmail.com>
2021-05-18 00:28:46 -0400

    it works

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Script to flatten a docker image into a single layer, preserving metadata like env vars and entrypoint.
+
+Usage:
+
+```shell
+# One-off invocation
+# -y suppresses confirmation prompt
+npx -y cspotcode/flatten-docker-image --help
+npx -y cspotcode/flatten-docker-image <image tag or ID>
+
+# To install globally
+npm install -g cspotcode/flatten-docker-image
+flatten-docker-image <image tag or ID>
+```
--- index.ts
+++ index.ts
@@ -3,14 +3,19 @@ type TODO = unknown;
 
 import fs from 'fs';
 import Path from 'path';
+import os from 'os';
 import execa from 'execa';
 import {Cli, Builtins, Command, Usage, BaseContext, Option} from 'clipanion';
+import { Console } from 'console';
+const shqModulePromise = eval('import("shq")');
+
+const {name: binaryName, version: binaryVersion} = require('./package.json');
 
 function main() {
     const [node, app, ...args] = process.argv;
     const cli = new Cli({
-        binaryName: require('./package.json').name,
-        binaryVersion: require('./package.json').version
+        binaryName,
+        binaryVersion
     });
     cli.register(FlattenImageCommand);
     cli.register(Builtins.HelpCommand);
@@ -18,7 +23,10 @@ function main() {
     cli.runExit(args, Cli.defaultContext);
 }
 
-const supportedDockerfileCommands = 'CMD|ENTRYPOINT|ENV|EXPOSE|ONBUILD|USER|VOLUME|WORKDIR';
+/*
+ * Supported dockerfile commands:
+ * CMD|ENTRYPOINT|ENV|EXPOSE|ONBUILD|USER|VOLUME|WORKDIR
+*/
 
 class FlattenImageCommand extends Command {
     static paths = [Command.Default];
@@ -27,9 +35,67 @@ class FlattenImageCommand extends Command {
     }
 
     image = Option.String();
+    tag = Option.String('-t,--tag');
+
+    console!: Console;
 
     async execute() {
-        this.context.stdout.write(`TODO flatten this image: ${ this.image }\n`);
+        this.console = new Console(this.context.stdout, this.context.stderr);
+        const {console, image, tag} = this;
+        const {default: shq} = await shqModulePromise;
+        const cleanupTasks: Array<() => void | Promise<void>> = [];
+        const log = console.error.bind(console);
+        try {
+            log(`Inspecting docker image ${image}...`);
+            const [result] = await Promise.allSettled([execa('docker', ['image', 'inspect', image], {
+                stdout: 'pipe',
+                stderr: 'inherit'
+            })]);
+            if(result.status === 'rejected') {
+                console.error('Image not found.  Did you forget to `docker pull`?');
+                console.error(result.reason.message);
+                return 1;
+            }
+            const [dockerInspect] = JSON.parse(result.value.stdout) as DockerInspectOutput;
+            log(`Image has ${ dockerInspect.RootFS.Layers.length} layers.`);
+            
+            log(`Creating temporary container...`);
+            const result2 = await execa('docker', ['container', 'create', dockerInspect.Id]);
+            const containerId = result2.stdout;
+            log(`Created temporary container ${containerId}`);
+            cleanupTasks.push(async () => {
+                log(`Deleting temporary container...`);
+                await execa('docker', ['container', 'rm', containerId]);
+            });
+
+            const tempDir = fs.mkdtempSync(Path.join(os.tmpdir(), binaryName));
+            const tempFilePath = Path.join(tempDir, 'export.tar');
+            cleanupTasks.push(async () => {
+                log(`Deleting temporary file ${ tempFilePath }...`);
+                fs.rmSync(tempFilePath);
+            });
+            log(`Exporting container filesystem to temporary file ${ tempFilePath }...`);
+            await execa('docker', ['container', 'export', '-o', tempFilePath, containerId], {
+                stdio: 'inherit'
+            });
+
+            const flags = buildDockerImportChangeFlags(dockerInspect);
+            log(`Computed set of Dockerfile changes to apply to imported image: ${flags.map(flag => shq(flag)).join(' ')}`);
+            log(`Importing exported filesystem into new image...`);
+            const result3 = await execa('docker', ['image', 'import', ...flags, tempFilePath], {
+                stderr: 'inherit'
+            });
+            const flattenedImageId = result3.stdout;
+            log(`Created image ${flattenedImageId}`);
+            if(tag) {
+                await execa('docker', ['image', 'tag', flattenedImageId, tag], {stdio: 'inherit'});
+                log(`Tagged image as ${tag}`);
+            }
+        } finally {
+            for(const task of cleanupTasks) {
+                await task();
+            }
+        }
     }
 }
 
@@ -45,13 +111,16 @@ interface DockerInspectStruct {
         OnBuild: null | TODO;
         User: string;
         // TODO add support for expose, onbuild, volume
+    };
+    RootFS: {
+        Type: 'layers';
+        Layers: string[];
     }
 }
 
-function buildDockerImportChangeFlags(dockerInspectOutput: DockerInspectOutput) {
-    const [dockerInspect] = dockerInspectOutput;
+function buildDockerImportChangeFlags(dockerInspect: DockerInspectStruct) {
     const {Cmd, Entrypoint, Env, OnBuild, User, Volumes, WorkingDir} = dockerInspect.Config;
-    const flags = [];
+    const flags: string[] = [];
     function addChange(change: string) {
         flags.push('--change', change);
     }
